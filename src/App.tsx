940ff8011e0de170c95c317add0c2c591d9fcc7d
McPixeI <torres.88.bcn@gmail.com>
2022-06-15 18:50:45 +0200

    added mutations

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,57 @@
 import React from "react";
-import "./App.css";
-import { useGetContactsQuery } from "./features/contacts/contactsApi";
+import {
+  useAddContactMutation,
+  useGetContactQuery,
+  useGetContactsQuery,
+} from "./features/contacts/contactsApi";
 
 function App() {
-  const { data, isLoading, isFetching, isSuccess, isError, error } =
+  const { data, isLoading, isFetching, isSuccess, isError } =
     useGetContactsQuery();
   return (
     <div className="App">
       {isLoading && <h2>Loading...</h2>}
       {isFetching && <h2>Fetching...</h2>}
-      {isError && <h2>{`Error: ${error}`}</h2>}
-      {isSuccess && data?.map((x) => <p>{x.name}</p>)}
+      {isError && <h2>Oopsie</h2>}
+      {isSuccess && (
+        <>
+          {data?.map((contact) => (
+            <>
+              <p>{contact.name}</p>
+              <ContactDetail id={contact.id} />
+            </>
+          ))}
+        </>
+      )}
+      <div>
+        <AddContact />
+      </div>
     </div>
   );
 }
+
+export const ContactDetail = ({ id }: { id: string }) => {
+  const { data } = useGetContactQuery(id);
+  return <pre>data: {JSON.stringify(data, undefined, 2)}</pre>;
+};
+
+export const AddContact = () => {
+  const [addContact] = useAddContactMutation();
+  const contact = {
+    id: "4",
+    name: "yisus",
+    email: "yisus@gmail.com",
+  };
+
+  const addHandler = async () => {
+    await addContact(contact);
+  };
+
+  return (
+    <>
+      <button onClick={addHandler}>Add new contact</button>
+    </>
+  );
+};
 
 export default App;
--- src/features/contacts/contactsApi.ts
+++ src/features/contacts/contactsApi.ts
@@ -4,14 +4,46 @@ import { Contact } from "../../models/contact.model";
 export const contactsApi = createApi({
   reducerPath: "contactsApi",
   baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_API_BASE_URL }),
+  tagTypes: ["Contact"],
   endpoints: (builder) => ({
     getContacts: builder.query<Contact[], void>({
       query: () => "contacts",
+      providesTags: ["Contact"],
     }),
     getContact: builder.query<Contact, string>({
       query: (id) => `contacts/${id}`,
+      providesTags: ["Contact"],
+    }),
+    addContact: builder.mutation<Contact, Partial<Contact>>({
+      query: (contact) => ({
+        url: "contacts",
+        method: "POST",
+        body: contact,
+      }),
+      invalidatesTags: ["Contact"],
+    }),
+    updateContact: builder.mutation<Contact, Partial<Contact>>({
+      query: ({ id, ...rest }) => ({
+        url: `contacts/${id}`,
+        method: "PUT",
+        body: rest,
+      }),
+      invalidatesTags: ["Contact"],
+    }),
+    deleteContact: builder.mutation<{ success: boolean; id: number }, string>({
+      query: (id) => ({
+        url: `contacts/${id}`,
+        method: "delete",
+      }),
+      invalidatesTags: ["Contact"],
     }),
   }),
 });
 
-export const { useGetContactsQuery, useGetContactQuery } = contactsApi;
+export const {
+  useGetContactsQuery,
+  useGetContactQuery,
+  useAddContactMutation,
+  useUpdateContactMutation,
+  useDeleteContactMutation,
+} = contactsApi;
